5d13c5dba166bce71cd03684ae0a520a39296e0b
Jishasherlin <jishasherlin15@gmail.com>
2025-12-04 13:56:36 +0530

    Remove Mentor model definition

--- Mentor.php
+++ Mentor.php
@@ -1,13 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Mentor extends Model
-{
-    use HasFactory;
-    protected $table = 'mentors';
-    protected $fillable = ['image','name','email','expertise'];
-}
