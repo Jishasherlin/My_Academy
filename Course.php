e6188d2625349fbc20fbde61768fb5774a60eec6
Jishasherlin <jishasherlin15@gmail.com>
2025-12-04 13:54:47 +0530

    Remove Course model implementation

--- Course.php
+++ Course.php
@@ -1,14 +1,1 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Course extends Model
-{
-    use HasFactory;
-
-protected $fillable=['title', 'description', 'duration', 'instructor', 'price', 'category', 'level', 'syllabus'];
-}
 
